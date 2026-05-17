9a9a6f024c2dae07605a9ee6e1842b2113733a4d
Nathan H <22816739+nathub@users.noreply.github.com>
2026-05-17 17:30:54 -0300

    beginning Haskell video

--- haskell_test/haskell_test.hs
+++ haskell_test/haskell_test.hs
@@ -1,11 +1,17 @@
--- testing pattern matching and guards I guess
-isXFactorOfY :: Num a => a -> a -> Bool
+-- testing pattern matching and guards
+--isXFactorOfY :: Num a => a -> a -> Bool MY definition, which caused compile error
+isXFactorOfY :: (RealFrac a, Integral a) => a -> a -> Bool -- GHCi 's suggested 
 isXFactorOfY 0 0 = True
 isXFactorOfY 0 y = False
 isXFactorOfY x 0 = False
 isXFactorOfY 1 1 = True
 isXFactorOfY 1 y = False
 isXFactorOfY x 1 = False
-isXFactorOfY x y	| x > y	= False
-			| abs(x / y) > floor( abs(x)/abs(y) ) = False
-			| otherwise = True
+isXFactorOfY x y        | x > y = False
+                        | abs(y / x) > floor( abs $ y / x ) = False
+                        | otherwise = True
+
+
+-- Haskell Programming Full Course 2024
+-- By BekBrace  https://youtu.be/TklkNLihQ_A
+
