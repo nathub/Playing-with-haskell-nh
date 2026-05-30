215f26c8ce854e50c5dc28a4e166345c0f35daea
Nathan H <22816739+nathub@users.noreply.github.com>
2026-05-30 18:45:21 -0300

    trying misc other videos now.

--- haskell_test.hs
+++ haskell_test.hs
@@ -1,5 +1,8 @@
+main :: IO()
+main = print "Main method!"
+
 -- testing pattern matching and guards
---isXFactorOfY :: Num a => a -> a -> Bool MY definition, which caused compile error
+-- isXFactorOfY :: Num a => a -> a -> Bool MY definition, which caused compile error
 isXFactorOfY :: (RealFrac a, Integral a) => a -> a -> Bool -- GHCi 's suggested , still broken
 isXFactorOfY 0 0 = True
 isXFactorOfY 0 y = False
@@ -11,3 +14,10 @@ isXFactorOfY x y        | x > y = False
                         | abs(y / x) > floor( abs $ y / x ) = False
                         | otherwise = True
 
+-- Watching peppidesu video https://www.youtube.com/watch?v=7awJDYMXtXE
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n
+    | n < 0 = 1
+    | otherwise = n * factorial (n-1)
