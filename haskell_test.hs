2dcc4a0451ef3661da72f949ddb07156715275a3
Nathan H <22816739+nathub@users.noreply.github.com>
2026-05-30 19:09:51 -0300

    struggling with elem example excercise

--- haskell_test.hs
+++ haskell_test.hs
@@ -16,8 +16,17 @@ isXFactorOfY x y        | x > y = False
 
 -- Watching peppidesu video https://www.youtube.com/watch?v=7awJDYMXtXE
 
-factorial :: Int -> Int
-factorial 0 = 1
-factorial n
+myfactorial :: Int -> Int
+myfactorial 0 = 1
+myfactorial n
     | n < 0 = 1
-    | otherwise = n * factorial (n-1)
+    | otherwise = n * myfactorial (n-1)
+
+mytake :: Integer -> [a] -> [a]
+mytake 0 _ = []
+mytake _ [] = []
+mytake n (x:xs) = x : mytake (n-1) xs
+
+myelem :: [a] -> a -> Bool
+myelem [] _ = False
+myelem (x:xs) d = d == x || myelem xs d 
