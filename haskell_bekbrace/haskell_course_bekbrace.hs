3bbf2d5cdb38a705470ccfe1e271c72ae596e549
Nathan H <22816739+nathub@users.noreply.github.com>
2026-05-30 18:31:01 -0300

    I didn't like that course

--- haskell_bekbrace/haskell_course_bekbrace.hs
+++
@@ -1,9 +0,0 @@
--- Haskell Programming Full Course 2024
--- By BekBrace  https://youtu.be/TklkNLihQ_A
-
-main :: IO()
-main = putStrLn "Hello, World!"
-
-celsiusToFarenheit c = (c * 9/5) + 32
-
--- I don't like this course
